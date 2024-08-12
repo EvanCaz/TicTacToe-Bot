f793fedc0d59fbdf529320835a0d10f3318f324a
Evan Cazares <evan.cazares@g.austincc.edu>
2024-08-12 15:08:29 -0500

    lookup table

--- Bot.java
+++ Bot.java
@@ -3,13 +3,12 @@ import java.util.Random;
 public class Bot {
     private static int turnCnt = 0;
     private static boolean hasCenter = false;
+    // private static int[][] lookup;
     // private static int[] lastMove = new int[2];
     public static void main(String[] args) {
         // Bot myBot = new Bot();
     }
-    /*
-     * If they dont take the center, take the center, if they do, take any one of four corners
-     */
+
     public void makeMove(char[][] board){
         if(turnCnt == 0){
             if(board[1][1] == '#'){
@@ -32,32 +31,16 @@ public class Bot {
         } else if (turnCnt == 1) { // if he has the center, we are basically playing defense unles he screws up
                 int x = game.pastMoves[0];
                 int y = game.pastMoves[1];    
-                // System.out.println("(" + x + "," + y + ")");
-                // System.out.println("Test" + hasCenter);
-                if(hasCenter == true){ // if they have the center, no matter where they place it we have to go to the oppsite
-                    if(x == 0 && y == 0){
-                        board[2][2] = 'o';
-                    }
-                    if(x == 0 && y == 1){
-                        board[2][1] = 'o';
-                    }
-                    if(x == 0 && y == 2){
-                        board[2][0] = 'o';
-                    }
-                    if(x == 1 && y == 0){
-                        board[1][2] = 'o';
-                    }
-                    if(x == 1 && y == 2){
-                        board[1][0] = 'o';
-                    }
-                    if(x == 2 && y == 0){
-                        board[0][2] = 'o';
-                    }
-                    if(x == 2 && y == 1){
-                        board[0][1] = 'o';
-                    }
-                    if(x == 2 && y == 2){
-                        board[0][0] = 'o';
+                if(hasCenter == true){ // if they have the center, no matter where they place it we have to go to the oppsite, single case we dont have to check in our last move is there
+                    int[][] lookup = {
+                        {2, 2, 2, 1, 2, 0}, // for x = 0, y = 0 maps to board[2][2], y = 1 maps to board[2][1], y = 2 maps to board[2][0]
+                        {1, 2, 1, 0, 1, 2}, // for x = 1, y = 0 maps to board[1][2], y = 1 maps to board[1][0], y = 2 maps to board[1][2]
+                        {0, 2, 0, 1, 0, 0}  // for x = 2, y = 0 maps to board[0][2], y = 1 maps to board[0][1], y = 2 maps to board[0][0]
+                    };
+                    if (x >= 0 && x < 3 && y >= 0 && y < 3) {
+                        int row = lookup[x][y];
+                        int col = 2 - y;  // Column inversion based on your specific mapping
+                        board[row][col] = 'o';
                     }
                 } else {
 

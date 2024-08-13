f0b9ef952d3ef54a64ea2c0f97db35912a115a7a
Evan Cazares <evan.cazares@g.austincc.edu>
2024-08-13 14:50:09 -0500

    all works execept game ending condtions

--- Bot.java
+++ Bot.java
@@ -9,7 +9,14 @@ public class Bot {
         // Bot myBot = new Bot();
     }
 
-    public void makeMove(char[][] board){
-        
+    public void randomMove(game newGame, char symbol){
+        Random rnd = new Random();
+        int randomIndex = rnd.nextInt(newGame.validMoves.size());
+        int pickedMove = newGame.validMoves.get(randomIndex);
+        System.out.println();
+        System.out.println(symbol + " picked index: " + pickedMove);
+        newGame.validMoves(pickedMove);
+        newGame.updateBoard(pickedMove, symbol);
     }
+        
 }
--- game.java
+++ game.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class game {
     private static final int spaces = 9;
     public static char board[] = new char[spaces];
+    public static List<Integer> validMoves = new ArrayList<>(); // for random chose bot
+    static private int tied = 0;
+    static private int xWins = 0;
+    static private int oWins = 0;
 
     public static void main (String[] args) {
         game newGame = new game();
-        // Bot newBot = new Bot();
-        newGame.printBoard();
-        newGame.getInput();
-        newGame.printBoard();
-        // while(newGame.gameOver() != true){
-        //     newGame.printBoard();
-        //     newGame.getInput();
-        //     newBot.makeMove(board);
-        // }
+        Bot bot1 = new Bot();
+        Bot bot2 = new Bot();
+        boolean game = false;
+        while(newGame.gameOver() == false){
+            newGame.printBoard();
+            // newGame.validMoves();
+            bot1.randomMove(newGame, 'x');
+            game = newGame.gameOver();
+            if(game == true){
+                break;
+            }
+            bot2.randomMove(newGame, 'o');
+        }
+        System.out.println("There were " + xWins + " number of x wins and there were " + oWins + " number of x wins and there were " + tied + " ties.");
         // prolly while loop until a winner is found, passing the board to a bot class for other move
     }
 
@@ -22,16 +33,15 @@ public class game {
         for(int i = 0; i < spaces; i++){  
                 board[i] = '-';
         }
+        for(int i = 0; i < spaces; i++){  
+            validMoves.add(i); // all moves are valid at the start
+    }
         // System.out.println("test");
     }
     
     private void printBoard() { // testing and shi
-        System.out.println("     0   1   2");
         int k = 0;
         for(int i = 0; i < spaces; i++){
-            if(i % 3 == 0) { 
-                System.out.print(i / 3 + " "); // row number
-            }
             System.out.print(" | " + board[i]);
             if((i + 1) % 3 == 0 && i != spaces - 1){
                 System.out.print(" |  ");
@@ -50,50 +60,81 @@ public class game {
         }
         System.out.print(" |");
     }
-    /*
-     * No input validation simply gets one after the other, this project is simply to demostrate the creation of a bot to play against
-     */
+
+    public void validMoves(int move){
+        validMoves.remove(Integer.valueOf(move));
+        // for (int validMove : validMoves) {
+        //     System.out.print(validMove + " ");
+        // }
+        // System.out.println();
+    }
+
     private void getInput() {
         Scanner scnr = new Scanner(System.in);
+        int cord = -1;
+        boolean success = false;
         System.out.println();
-        System.out.print("Enter x coordinate: ");
-        int cord = scnr.nextInt();
+        while(!success) {
+            System.out.print("Enter coordinate: ");
+            try {
+                cord = scnr.nextInt();
+                if((cord >= 0 && cord <= 8) && (validMoves.contains(cord) == true)) {
+                    success = true;
+                } else {
+                    System.out.println("Enter a number within range and is not already used.");
+                }
+            } catch (Exception e) {
+                System.out.println("Enter a valid number.");
+                scnr.nextLine(); // Clear the invalid input
+            }
+        }
         updateBoard(cord, 'X');
-        
-        
+        validMoves(cord);
     }
+
 
     public void updateBoard(int cord, char symbol){
         board[cord] = symbol;
     }
 
-    /*
-     * Checks all row and if one is equal, game over
-     * checks all cols and if one is equal, game over
-     * checks both diagonals and if one is equal, game is over, should probably check first for efficieny
-     * check if board is full, which would iterate over the whole thing and should be done last
-     * breaks the rule of more than one return statmenet as this is more efficnet  
-     */
-    // private boolean gameOver() { 
-    //     if (board[0][0] != '#' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-    //         System.out.println("Player " + board[0][0] + " Wins");
-    //         return true;
-    //     }
-    //     if (board[0][2] != '#' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-    //         System.out.println("Player " + board[0][2] + " Wins");
-    //         return true;
-    //     }
-    //     for(int i = 0; i < rows; i++){ // check all rows if they are equal to each other
-    //         if(board[i][0] != '#' && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
-    //             System.out.println("Player " + board[i][0] + " Wins");
-    //             return true;
-    //         }
-    //         if(board[0][i] != '#' && board[0][i] == board[1][i] && board[1][i] == board[2][i]){
-    //             System.out.println("Player " + board[0][i] + " Wins");
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
+    private boolean gameOver() {
+        int[][] winningCombinations = {
+            {0, 1, 2}, // Row 1
+            {3, 4, 5}, // Row 2
+            {6, 7, 8}, // Row 3
+            {0, 3, 6}, // Column 1
+            {1, 4, 7}, // Column 2
+            {2, 5, 8}, // Column 3
+            {0, 4, 8}, // Diagonal from top-left to bottom-right
+            {2, 4, 6}  // Diagonal from top-right to bottom-left
+        };
 
+        char winner = '-';
+        for (int[] combination : winningCombinations) {
+            if (board[combination[0]] != '-' && 
+                board[combination[0]] == board[combination[1]] && 
+                board[combination[1]] == board[combination[2]]) {
+                winner = board[combination[0]];
+                break;
+            }
+        }
+
+        if (winner == 'X') {
+            xWins++;
+            return true;
+        } else if (winner == 'O') {
+            oWins++;
+            return true;
+        }
+        
+        // Check for a tie condition (no winner and board is full)
+        for (char cell : board) {
+            if (cell == '-') {
+                return false; // Empty cell found, game is not over
+            }
+        }
+        
+        // If no winner and no empty cells, it's a tie
+        return true;
+    }
 }

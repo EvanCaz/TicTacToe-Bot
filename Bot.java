12a83d32dbbd825570fcc9630700970f001d16ae
Evan Cazares <evan.cazares@g.austincc.edu>
2024-08-13 13:20:12 -0500

    reformatted the game so it can be easily read by an alogirthm

--- Bot.java
+++ Bot.java
@@ -1,7 +1,5 @@
 import java.util.Random;
 
-import javax.sound.midi.SysexMessage;
-
 public class Bot {
     private static int turnCnt = 0;
     private static boolean hasCenter = false;
@@ -12,103 +10,6 @@ public class Bot {
     }
 
     public void makeMove(char[][] board){
-        if(turnCnt == 0){
-            if(board[1][1] == '#'){
-                board[1][1] = 'o';
-                turnCnt++;
-            } else { // choose a four corner, id like it to be a random corner, so the games are not repetitive
-                Random rnd = new Random();
-                int[] values = {0, 2};
-                int rndIndex = rnd.nextInt(values.length);
-                int rndCol = values[rndIndex];
-                rndIndex = rnd.nextInt(values.length);
-                int rndRow = values[rndIndex];
-                lastMove[0] = rndRow;
-                lastMove[1] = rndCol;
-                System.out.println("player chose (1,1), choosing: (" + rndRow + "," + rndCol+ ")");    
-                board[rndRow][rndCol] = 'o';
-                hasCenter = true;
-                turnCnt++;
-            }
-        } else if (turnCnt == 1) { 
-                int x = game.pastMoves[0];
-                int y = game.pastMoves[1];    
-                if(hasCenter == true){   // if they have the center, no matter where they place it we have to go to the oppsite, single case we dont have to check in our last move is there
-                    int[][] lookup = {
-                        {2, 2, 2, 1, 2, 0}, // for x = 0, y = 0 maps to board[2][2], y = 1 maps to board[2][1], y = 2 maps to board[2][0]
-                        {1, 2, 1, 0, 1, 2}, // for x = 1, y = 0 maps to board[1][2], y = 1 maps to board[1][0], y = 2 maps to board[1][2]
-                        {0, 2, 0, 1, 0, 0}  // for x = 2, y = 0 maps to board[0][2], y = 1 maps to board[0][1], y = 2 maps to board[0][0]
-                    };
-                    if (x >= 0 && x < 3 && y >= 0 && y < 3) {
-                        int row = lookup[x][y];
-                        int col = 2 - y;  //  inversion 
-                        System.out.println(board[row][col]); // before a spot is chosen
-                        if(row == 2 && col == 1){  // weird case
-                            board[0][1] = 'o';
-                        } else {
-                            if(board[row][col] != 'o'){
-                                board[row][col] = 'o';
-                            } else { // if we have already blocked it, pick remaining corner, this could also be randomized for non repetitive games
-                                System.out.println("Player last move: (" + x + "," + y + ")");
-                                System.out.println("Computer last move: (" + lastMove[0] + "," + lastMove[1] + ")");
-                                if((x == 2 && y == 0) && (lastMove[0] == 0 && lastMove[1] == 2) || ((x == 0 && y == 2) && (lastMove[0] == 2 && lastMove[1] == 0))){ // if both bottom left and top right corners taken, choose top left
-                                    board[0][0] = 'o';
-                                } else if ((x == 0 && y == 0) && (lastMove[0] == 2 && lastMove[1] == 2) || ((x == 2 && y == 2) && (lastMove[0] == 2 && lastMove[1] == 2))){ // oppsite of above
-                                    board[0][2] = 'o';
-                                }
-                            }
-                        }
-                    }
-                } else { // if bot has center
-                    /*
-                    *  if his second move allows him to win, block it. This case occurs when there are to 'x's in the same row or column, as we have the center piece
-                    *  otherwise we can win
-                    */
-                    
-                    // System.out.println("Test center reached"); 
-                    int j = 0; int k = 0;
-                    int tmpX = 0, tmpY = 0;
-                    for(int i = 0; i < 3; i++){
-                        if(board[0][i] == 'x'){
-                            System.out.println("test");
-                            if(board[0][i] == '#'){
-                                System.out.println("test");
-                                tmpY = i;
-                                
-                            }
-                            j++;
-                        }  
-                        if (board[2][i] == 'x'){
-                            if(board[i][0] == '#'){
-                                System.out.println(i);
-                                tmpY = i;
-                            }
-                            j++;
-                        }
-                        // if(board[i][0] == 'x' || board[i][2] == 'x'){
-                        //     if(board[0][i] == '#'){
-                        //         tmpY = i;
-                        //     } else {
-                        //         tmpX = 2;
-                        //     }
-                        //     k++;
-                        // }
-                    }
-                    if(j > 1){
-                        // board[0][temp] = 'o';
-                        System.out.println("in the same row detected 2");
-                        System.out.println(tmpY);
-                    } else if (k > 1) {
-                        // board[tmpX][tmpY] = 'o';
-                        System.out.println("in the same col detected 2");
-                        System.out.println(tmpX);
-                    }
-                }
-                turnCnt++;
-        } else if (turnCnt > 1){
-            // if it is greater than one and no winners, i think we cant win and must just play defense
-            System.out.println("Test reach"); 
-        }
-
+        
     }
 }
--- game.java
+++ game.java
@@ -1,61 +1,70 @@
 import java.util.Scanner;
 
 public class game {
-    private static final int rows = 3;
-    private static final int cols = 3;
-    public static char board[][] = new char[rows][cols];
-    public static int[] pastMoves = new int[2];
+    private static final int spaces = 9;
+    public static char board[] = new char[spaces];
 
     public static void main (String[] args) {
         game newGame = new game();
-        Bot newBot = new Bot();
-        while(newGame.gameOver() != true){
-            newGame.printBoard();
-            newGame.getInput();
-            newBot.makeMove(board);
-        }
+        // Bot newBot = new Bot();
+        newGame.printBoard();
+        newGame.getInput();
+        newGame.printBoard();
+        // while(newGame.gameOver() != true){
+        //     newGame.printBoard();
+        //     newGame.getInput();
+        //     newBot.makeMove(board);
+        // }
         // prolly while loop until a winner is found, passing the board to a bot class for other move
     }
 
     public game () {
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                board[i][j] = '#';
-            }
+        for(int i = 0; i < spaces; i++){  
+                board[i] = '-';
         }
         // System.out.println("test");
     }
     
     private void printBoard() { // testing and shi
-        System.out.println("    0   1   2");
-        for(int i = 0; i < rows; i++){
-            System.out.print(i + " | ");
-            for(int j = 0; j < cols; j++){
-                System.out.print(board[i][j] + " | ");
+        System.out.println("     0   1   2");
+        int k = 0;
+        for(int i = 0; i < spaces; i++){
+            if(i % 3 == 0) { 
+                System.out.print(i / 3 + " "); // row number
             }
-            System.out.println();
+            System.out.print(" | " + board[i]);
+            if((i + 1) % 3 == 0 && i != spaces - 1){
+                System.out.print(" |  ");
+                for(int j = 0; j < 3; j++){
+                    System.out.print(" | " + k);
+                    k++;
+                }
+                System.out.print(" |");
+                System.out.println();
+            }
+        } 
+        System.out.print(" |  ");
+        for(int j = 0; j < 3; j++){
+            System.out.print(" | " + k);
+            k++;
         }
-        
-        
+        System.out.print(" |");
     }
     /*
      * No input validation simply gets one after the other, this project is simply to demostrate the creation of a bot to play against
      */
     private void getInput() {
         Scanner scnr = new Scanner(System.in);
+        System.out.println();
         System.out.print("Enter x coordinate: ");
-        int xCord = scnr.nextInt();
-        pastMoves[0] = xCord;
-        System.out.print("Enter y coordinate: ");
-        int yCord = scnr.nextInt();
-        pastMoves[1] = yCord;
-        // System.out.println(xCord + " : " + yCord);
-        updateBoard(xCord, yCord, 'x');
-
+        int cord = scnr.nextInt();
+        updateBoard(cord, 'X');
+        
+        
     }
 
-    public void updateBoard(int xCord, int yCord, char symbol){
-        board[xCord][yCord] = symbol;
+    public void updateBoard(int cord, char symbol){
+        board[cord] = symbol;
     }
 
     /*
@@ -65,26 +74,26 @@ public class game {
      * check if board is full, which would iterate over the whole thing and should be done last
      * breaks the rule of more than one return statmenet as this is more efficnet  
      */
-    private boolean gameOver() { 
-        if (board[0][0] != '#' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-            System.out.println("Player " + board[0][0] + " Wins");
-            return true;
-        }
-        if (board[0][2] != '#' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-            System.out.println("Player " + board[0][2] + " Wins");
-            return true;
-        }
-        for(int i = 0; i < rows; i++){ // check all rows if they are equal to each other
-            if(board[i][0] != '#' && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
-                System.out.println("Player " + board[i][0] + " Wins");
-                return true;
-            }
-            if(board[0][i] != '#' && board[0][i] == board[1][i] && board[1][i] == board[2][i]){
-                System.out.println("Player " + board[0][i] + " Wins");
-                return true;
-            }
-        }
-        return false;
-    }
+    // private boolean gameOver() { 
+    //     if (board[0][0] != '#' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+    //         System.out.println("Player " + board[0][0] + " Wins");
+    //         return true;
+    //     }
+    //     if (board[0][2] != '#' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+    //         System.out.println("Player " + board[0][2] + " Wins");
+    //         return true;
+    //     }
+    //     for(int i = 0; i < rows; i++){ // check all rows if they are equal to each other
+    //         if(board[i][0] != '#' && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+    //             System.out.println("Player " + board[i][0] + " Wins");
+    //             return true;
+    //         }
+    //         if(board[0][i] != '#' && board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+    //             System.out.println("Player " + board[0][i] + " Wins");
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 
 }

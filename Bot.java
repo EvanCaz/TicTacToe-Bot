7e3600599429ab1941b6df9229b46997e4c14c7f
Evan Cazares <evan.cazares@g.austincc.edu>
2024-08-17 00:29:43 -0500

    algorithm working and in the bot class, i was just stupid

--- Bot.java
+++ Bot.java
@@ -8,181 +8,77 @@ import java.lang.Math;
 
 public class Bot {
     private char symbol;
-    Map<Character, Integer> scores = new HashMap<>();
-    public static void main(String[] args) {
-        // Bot myBot = new Bot();
-    }
+    int topMove;
 
     public Bot(char x){
         symbol = x;
     }
 
-    public void pickRandom(game newGame){
-        Random rnd = new Random();
-        int randomIndex = rnd.nextInt(newGame.availMoves.size());
-        int pickedMove = newGame.availMoves.get(randomIndex);
-        System.out.print(symbol + " picked " + pickedMove + ". ");
-        newGame.validMoves(pickedMove);
-        newGame.updateBoard(pickedMove, symbol);
-    }
-
-    public void pickCenter(game newGame){ // if the center is open, pick it
-        if(newGame.availMoves.contains(4)){
-            System.out.print(symbol + " picked the center. ");
-            newGame.validMoves(4);
-            newGame.updateBoard(4, symbol);
-            
-        } else {
-            pickRandom(newGame);
-        }
-    }
-
-    // public void pickWinCen(game newGame){
-    //     boolean moveFound = false;
-    //     for(int i = 1; i < newGame.availMoves.size(); i++){ 
-    //         int move = newGame.availMoves.get(i);
-    //         newGame.updateBoard(move, symbol);
-    //         if(newGame.gameOver() == true){ 
-    //             moveFound = true;
-    //             newGame.validMoves(move);
-    //             newGame.updateBoard(move, symbol);
-    //             System.out.print(symbol + " picked winning move " + move + ". ");
-    //             break;
-    //         } else {
-    //         newGame.updateBoard(move, '-');
-    //         }
-    //     }
-    //     if(moveFound == false){
-    //         pickCenter(newGame);
-    //     }
-    // }
-
     public void bestMove(game newGame){
-        int bestScore = 0;
-        int topMove = -1; // this is the best move itself, not the index in the arraylist availmove. 
-        for(int i = 0; i < newGame.availMoves.size(); i++){
-            // make each move
-            int move = newGame.availMoves.get(i);
-            newGame.board[move] = symbol; // make the move
-            int curScore = miniMax(newGame, 0, false); // pass the move
-            newGame.board[move] = '-'; // undo the move
-            if(curScore > bestScore){
-                bestScore = curScore;
-                topMove = move;
+        int bestScore = Integer.MIN_VALUE;
+        int topMove = -1;
+        for(int i = 0; i < newGame.spaces; i++){
+                if(newGame.board[i] != 'x' && newGame.board[i] != 'o'){
+                newGame.board[i] = symbol;
+                int curScore = miniMax(newGame, 0, false);
+                newGame.board[i] = '-';
+
+                if (curScore > bestScore) {
+                    bestScore = curScore;
+                    topMove = i;
+                }
             }
         }
-        // newGame.validMoves(topMove);
-        // newGame.updateBoard(topMove, symbol);
-        System.out.println(topMove);
+        newGame.board[topMove] = symbol;
     }
-    private int miniMax(game newGame, int depth, boolean isMax){
-        // first check if it is a terminal state 
-        int bestScore = -1; // there should always be a tieing move so i shouldnt have to worry about initilizing this to -1
+    /*
+     * Errors trying to get this to run with availMoves and outside of class, so moved into here and kept very simple
+     */
+    private int miniMax(game newGame, int depth, boolean isMax){ // breaks rules Alexander Katrompas taught me
         char result = newGame.gameOver();
-        if(result != 'n'){ // if it is a terminal state, such as a winner or a draw, return the score associated
-            if(result == 'c'){
-                return 0;
-            } else if (isMax == true) {
-                return 5;
+        if(newGame.gameOver() == 'c' || newGame.gameOver() == 'x' || newGame.gameOver() == 'o'){
+            if (result == 'c') {
+                return 0; // Draw
+            } else if (result == symbol) {
+                return 10; // Bot wins
             } else {
-                return -5;
+                return -10; // Opponent wins
             }
         }
-        if(isMax) { // if we are the max player
-            // check all the possible spots again, but when we recurse, the isMax is false
-            for(int i = 0; i < newGame.availMoves.size(); i++){
-                int move = newGame.availMoves.get(i);
-                newGame.board[move] = symbol;
-                int curScore = miniMax(newGame, depth + 1, false);
-                newGame.board[move] = '-';
-                bestScore = Math.max(curScore, bestScore);
-            }  
-            return bestScore; 
-        } else { // if it is the minimizing players turn, pass the oppsite of x or o
-            for(int i = 0; i < newGame.availMoves.size(); i++){
-                int move = newGame.availMoves.get(i);
-                newGame.board[move] = symbol;
-                int curScore = miniMax(newGame, depth + 1, true);
-                newGame.board[move] = '-';
-                bestScore = Math.min(curScore, bestScore);
+        if(isMax){
+            int bestScore = Integer.MIN_VALUE;
+            for(int i = 0; i < newGame.spaces; i++){
+                if(newGame.board[i] != 'x' && newGame.board[i] != 'o'){
+                    newGame.board[i] = symbol;
+                    int curScore = miniMax(newGame, depth + 1, false);
+                    newGame.board[i] = '-';
+                    bestScore = Math.max(curScore, bestScore);
+                }
             }
-            return bestScore;  
-        }    
+            return bestScore;
+        } else {
+            int bestScore = Integer.MAX_VALUE;
+            for(int i = 0; i < newGame.spaces; i++){
+                if(newGame.board[i] != 'x' && newGame.board[i] != 'o'){
+                    newGame.board[i] = (symbol == 'x') ? 'o' : 'x'; // Opponent's symbol
+                    int curScore = miniMax(newGame, depth + 1, true);
+                    newGame.board[i] = '-';
+                    bestScore = Math.min(curScore, bestScore);
+                }
+            }
+            return bestScore;
+        }
+
     }
-
-    // public void pickBlock(game newGame){
-    //     boolean moveFound = false;
-    //     for(int i = 0; i < newGame.availMoves.size(); i++){ 
-    //         int move = newGame.availMoves.get(i);
-    //         newGame.updateBoard(move, this.symbol);
-    //         if(this.symbol == 'x'){
-    //             newGame.updateBoard(move, 'o');
-    //         }
-    //         if(this.symbol == 'o'){
-    //             newGame.updateBoard(move, 'x');
-    //         }
-    //         if(newGame.gameOver() == true){
-    //             // moveFound = true;
-    //             System.out.println(symbol +"picked could pick blocking move " + move);
-    //             break;
-    //         } 
-    //         newGame.updateBoard(move, '-');
-    //     }
-    //     if(moveFound == false){
-    //         pickRandom(newGame);
-    //     }
+    
+    // public void pickRandom(game newGame){
+    //     Random rnd = new Random();
+    //     int randomIndex = rnd.nextInt(newGame.availMoves.size());
+    //     int pickedMove = newGame.availMoves.get(randomIndex);
+    //     System.out.print(symbol + " picked " + pickedMove + ". ");
+    //     newGame.validMoves(pickedMove);
+    //     newGame.updateBoard(pickedMove, symbol);
     // }
-}   
-    /*
-     * If the center has been picked, pick a corner, else pick random. This is tactic used in real tictactoe as if u start second
-     * You want to pick the corners if the first player picked a center
-     */
-    // public void pickCorner(game newGame){ // if center has been picked, pick a corner, else pick random. 
-    //     if(newGame.availMoves.contains(4) == false && firstPicked == false){ // if the center has been picked
-    //         ArrayList<Integer> availCorners = new ArrayList<>();
-    //         firstPicked = true;
-    //         if(newGame.availMoves.contains(0)) availCorners.add(0);
-    //         if(newGame.availMoves.contains(2)) availCorners.add(2);
-    //         if(newGame.availMoves.contains(6)) availCorners.add(6);
-    //         if(newGame.availMoves.contains(8)) availCorners.add(8);
-    //         // for(int i : availCorners){
-    //         //     System.out.print(i + ", ");
-    //         // }
-    //         if(availCorners.isEmpty() == false){
-    //             Random rnd = new Random();
-    //             int randomCorner = availCorners.get(rnd.nextInt(availCorners.size()));
-    //             // int index = newGame.availMoves.get(randomCorner);
-    //             newGame.validMoves(randomCorner);
-    //             newGame.updateBoard(randomCorner, symbol);
-    //             // System.out.print(symbol + " picked the corner " + randomCorner + ". ");
-    //         } 
-    //     } else {
-    //         pickRandom(newGame);    
-    //     }
-    // }
+}
 
-    /*
-     * for each move in the valid moves list, make them and see if the result in a win, if they do make it, 
-     * if none do, revert and make a random move.
-     */
-    // public void pickWinning(game newGame){
-    //     boolean moveFound = false;
-    //     for(int i = 1; i < newGame.availMoves.size(); i++){ 
-    //         int move = newGame.availMoves.get(i);
-    //         newGame.updateBoard(move, symbol);
-    //         if(newGame.gameOver() == true){ // incorrectly counts a full board as winning for this bot
-    //             moveFound = true;
-    //             newGame.validMoves(move);
-    //             newGame.updateBoard(move, symbol);
-    //             // System.out.print(symbol + " picked winning move " + move + ". ");
-    //             break;
-    //         } else {
-    //         newGame.updateBoard(move, '-');
-    //         }
-    //     }
-    //     if(moveFound == false){
-    //         pickRandom(newGame);
-    //     }
-    // }
 
-        
--- game.java
+++ game.java
@@ -1,73 +1,98 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Math;
 
 public class game {
-    public static final int spaces = 9;
+    public final int spaces = 9;
     public char board[] = new char[spaces];
     public List<Integer> availMoves = new ArrayList<>(); // for random chose bot
     static private int ties = 0;
     static private int xWins = 0;
     static private int oWins = 0;
+    
 
     public static void main (String[] args) {
-        // board[0] = 'x';
-        Bot bot1 = new Bot('o');
         game newGame =  new game();
-        newGame.board[2] = 'x';
-        newGame.board[3] = 'x';
-        newGame.board[6] = 'x';
-        newGame.board[0] = 'o';
-        newGame.board[7] = 'o';
-        newGame.board[8] = 'o';
+        Bot bot1 = new Bot('x');
+        Bot bot2 = new Bot('o');
+        while(newGame.gameOver() == 'n'){
+            newGame.printBoard();
+            try {
+                bot1.bestMove(newGame);
+            } catch (Exception e){
+                ties++;
+            }
+            newGame.printBoard();
+            try {
+                bot2.bestMove(newGame);
+            } catch (Exception e){
+                ties++;
+            }
+        }
         newGame.printBoard();
-        bot1.bestMove(newGame);
-
-
-
-
-        // // Bot bot2 = new Bot('o');
-        // boolean game;
-        // long totalTime = 0;
-        // int numGames = 1;
-        // System.out.println("Loading...");
-        // for(int i = 0; i < numGames; i++){
-        //     Bot bot1 = new Bot('x');
-        //     Bot bot2 = new Bot('o');
-        //     game newGame = new game();
-        //     game = false;
-        //     long startTime = System.currentTimeMillis();
-        //     while(game == false){
-        //         bot1.pickRandom(newGame);
-        //         game = newGame.gameOver();
-        //         if(game == true){
-        //             xWins++;
-        //             break;
-        //         }
-        //         System.out.print("Print x.");
-        //         newGame.printBoard(); 
-        //         bot2.pickRandom(newGame);
-        //         game = newGame.gameOver();
-        //         if(game == true){
-        //             oWins++;
-        //             break;
-        //         }
-        //         System.out.print("Print o.");
-        //         newGame.printBoard(); 
-        //     }
-        //     long endTime = System.currentTimeMillis();
-        //     long elapsed = endTime - startTime;
-        //     totalTime += elapsed;
-        //     // System.out.println();
-        //     newGame.printBoard();
-        // }
         
-        // double avgTime = (double) totalTime / numGames; // num games is first number
-        // System.out.println();
-        // System.out.println("There were " + String.format("%.4f", ((double) xWins / numGames) * 100) + "% number of x wins and there were " + String.format("%.4f", ((double) oWins / numGames) * 100) + "% o wins and " + String.format("%.4f", ((double) ties / numGames) * 100) + "% ties.");
-        // System.out.println("Average time of " + avgTime + " miliseconds.");
-        // // prolly while loop until a winner is found, passing the board to a bot class for other move
+
     }
+
+    // public void bestMove(){
+    //     int bestScore = Integer.MIN_VALUE;
+    //     int topMove = -1;
+    //     for(int i = 0; i < spaces; i++){
+    //             if(board[i] != 'x' && board[i] != 'o'){
+    //             board[i] = 'o';
+    //             int curScore = miniMax(board, 0, false);
+    //             board[i] = '-';
+
+    //             if (curScore > bestScore) {
+    //                 bestScore = curScore;
+    //                 topMove = i;
+    //             }
+    //         }
+    //     }
+    //     board[topMove] = 'o';
+
+    // }
+    // /*
+    //  * Errors trying to get this to run with availMoves and outside of class, so moved into here and kept very simple
+    //  */
+    // private int miniMax(char[] board, int depth, boolean isMax){ // breaks rules Alexander Katrompas taught me
+    //     char result = gameOver();
+    //     if(gameOver() == 'c' || gameOver() == 'x' || gameOver() == 'o'){
+    //         if (result == 'c') {
+    //             return 0; // Draw
+    //         } else if (result == 'o') {
+    //             return 10; // Bot wins
+    //         } else {
+    //             return -10; // Opponent wins
+    //         }
+    //     }
+    //     if(isMax){
+    //         int bestScore = Integer.MIN_VALUE;
+    //         for(int i = 0; i < spaces; i++){
+    //             if(board[i] != 'x' && board[i] != 'o'){
+    //                 board[i] = 'o';
+    //                 int curScore = miniMax(board, depth + 1, false);
+    //                 board[i] = '-';
+    //                 bestScore = Math.max(curScore, bestScore);
+    //             }
+    //         }
+    //         return bestScore;
+    //     } else {
+    //         int bestScore = Integer.MAX_VALUE;
+    //         for(int i = 0; i < spaces; i++){
+    //             if(board[i] != 'x' && board[i] != 'o'){
+    //                 board[i] = 'x';
+    //                 int curScore = miniMax(board, depth + 1, true);
+    //                 board[i] = '-';
+    //                 bestScore = Math.min(curScore, bestScore);
+    //             }
+    //         }
+    //         return bestScore;
+    //     }
+    // }
+
+    
 
     public game () {
         for(int i = 0; i < spaces; i++){  
@@ -75,8 +100,7 @@ public class game {
         }
         for(int i = 0; i < spaces; i++){  
             availMoves.add(i); // all moves are valid at the start
-    }
-        // System.out.println("test");
+        }
     }
     
     private void printBoard() { // testing and shi
@@ -103,42 +127,18 @@ public class game {
         System.out.println();
     }
 
+    public void printAvailMoves(){
+        for(int i : availMoves){
+            System.out.print(availMoves.get(i) + ", ");
+        }
+        System.out.println();
+    }
+
     public void validMoves(int move){
         availMoves.remove(Integer.valueOf(move));
-        // for (int validMove : validMoves) {
-        //     System.out.print(validMove + " ");
-        // }
-        // System.out.println();
     }
 
-    // private void getInput() {
-    //     Scanner scnr = new Scanner(System.in);
-    //     int cord = -1;
-    //     boolean success = false;
-    //     System.out.println();
-    //     while(!success) {
-    //         System.out.print("Enter coordinate: ");
-    //         try {
-    //             cord = scnr.nextInt();
-    //             if((cord >= 0 && cord <= 8) && (validMoves.contains(cord) == true)) {
-    //                 success = true;
-    //             } else {
-    //                 System.out.println("Enter a number within range and is not already used.");
-    //             }
-    //         } catch (Exception e) {
-    //             System.out.println("Enter a valid number.");
-    //             scnr.nextLine(); // Clear the invalid input
-    //         }
-    //     }
-    //     updateBoard(cord, 'X');
-    //     validMoves(cord);
-    // }
-
-
-    public void updateBoard(int cord, char symbol){
-        board[cord] = symbol;
-    }
-
+    
     public char gameOver() { 
         int[][] winningCombinations = {
             {0, 1, 2}, // Row 1
@@ -166,4 +166,26 @@ public class game {
         xWins--;
         return 'c';
     } 
+    private void getInput() {
+        Scanner scnr = new Scanner(System.in);
+        int cord = -1;
+        boolean success = false;
+        System.out.println();
+        while(!success) {
+            System.out.print("Enter coordinate: ");
+            try {
+                cord = scnr.nextInt();
+                if((cord >= 0 && cord <= 8) && (board[cord] == '-')) {
+                    success = true;
+                } else {
+                    System.out.println("Enter a number within range and is not already used.");
+                }
+            } catch (Exception e) {
+                System.out.println("Enter a valid number.");
+                scnr.nextLine(); // Clear the invalid input
+            }
+        }
+        board[cord] = 'x';
+        validMoves(cord);
+    }
 }
